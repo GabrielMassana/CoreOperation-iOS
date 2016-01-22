2bc04268e6256d92fea3f6b71396a97710a2abb3
GabrielMassana <gabrielmassana@gmail.com>
2016-01-22 11:37:02 +0000

    documentation

--- CoreOperation-iOS/Operation/COMOperation.h
+++ CoreOperation-iOS/Operation/COMOperation.h
@@ -12,16 +12,47 @@ typedef void (^COMOperationOnSuccess)(id result);
 typedef void (^COMOperationOnFailure)(NSError *error);
 typedef void (^COMOperationOnCompletion)(id result);
 
+/**
+ Subclass of NSOperation.
+ */
 @interface COMOperation : NSOperation
 
+/**
+ Operation identifier.
+ */
 @property (atomic, copy) NSString *identifier;
+
+/**
+ This is the identifier of the operation queue you would like the
+ operation to be run on.
+ */
 @property (atomic, copy) NSString *operationQueueIdentifier;
 
+/**
+ Callback called when the operation completes succesfully.
+ */
 @property (nonatomic, copy) COMOperationOnSuccess onSuccess;
+
+/**
+ Callback called when the operation fails.
+ */
 @property (nonatomic, copy) COMOperationOnFailure onFailure;
+
+/**
+ Callback called when the operation completes.
+ 
+ The completion block is used instead of the success/failure blocks not alongside.
+ */
 @property (nonatomic, copy) COMOperationOnCompletion onCompletion;
 
+/**
+ The result of the operation.
+ */
 @property (nonatomic, strong, readonly) id result;
+
+/**
+ The error of the operation.
+ */
 @property (nonatomic, strong, readonly) NSError *error;
 
 /**
